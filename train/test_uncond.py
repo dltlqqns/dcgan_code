00da3d1ea3abb8b2a76b54b820dbfc0e08f11ba9
Yumin <n12345@snu.ac.kr>
2017-06-29 01:33:09 +0900

    test_uncond debugged

--- train/test_uncond.py
+++ train/test_uncond.py
@@ -12,17 +12,16 @@ from lib.theano_utils import floatX, sharedX
 from sklearn.externals import joblib
 from inception_score import get_inception_score
 
-IMG_SIZE = 128
+IMG_SIZE = 64
 nc = 3            # # of channels in image
 npx = IMG_SIZE          # # of pixels width/height of images
 nz = 100          # # of dim for Z
 ngf = 1024         # # of gen filters in first conv layer
-ndf = 64         # # of discrim filters in first conv layer
 nx = npx*npx*nc   # # of dimensions in X
-LOAD_GEN_PATHS = ['./models/_CUB_200_2011_uncond_dcgan_/200_gen_params.jl', \
-                  './models/_CUB_200_2011_uncond_dcgan_/400_gen_params.jl', \
-                  './models/_CUB_200_2011_uncond_dcgan_/600_gen_params.jl', \
-                  './models/_CUB_200_2011_uncond_dcgan_/800_gen_params.jl']
+LOAD_GEN_PATHS = ['./models/_google_car_uncond_dcgan_truck_tmp/200_gen_params.jl', \
+                  './models/_google_car_uncond_dcgan_truck_tmp/400_gen_params.jl', \
+                  './models/_google_car_uncond_dcgan_truck_tmp/600_gen_params.jl', \
+                  './models/_google_car_uncond_dcgan_truck_tmp/800_gen_params.jl']
 NUM_SAMPLE = 500
 
 
@@ -42,7 +41,7 @@ gb = bias_ifn((ngf*4*4), 'gb')
 gw2 = gifn((ngf, ngf/2, 5, 5), 'gw2')
 gg2 = gain_ifn((ngf/2), 'gg2')
 gb2 = bias_ifn((ngf/2), 'gb2')
-gw3 = gifn((ngf/2, ngf*2, 5, 5), 'gw3')
+gw3 = gifn((ngf/2, ngf/4, 5, 5), 'gw3')
 gg3 = gain_ifn((ngf/4), 'gg3')
 gb3 = bias_ifn((ngf/4), 'gb3')
 gw4 = gifn((ngf/4, ngf/8, 5, 5), 'gw4')
@@ -62,16 +61,32 @@ if IMG_SIZE>=128:
 gwx = gifn((ng_final, nc, 5, 5), 'gwx')
 
 gen_params = [gw, gg, gb, gw2, gg2, gb2, gw3, gg3, gb3, gw4, gg4, gb4, gwx]
+if IMG_SIZE>=128:
+    gen_params.insert(-1, gw5)
+    gen_params.insert(-1, gg5)
+    gen_params.insert(-1, gb5)
+    if IMG_SIZE==256:    
+        gen_params.insert(-1, gw6)
+        gen_params.insert(-1, gg6)
+        gen_params.insert(-1, gb6)
 
-#def gen(Z, w, g, b, w2, g2, b2, w3, g3, b3, w4, g4, b4, wx):
-def gen(Z, w, g, b, w2, g2, b2, w3, g3, b3, w4, g4, b4, w5, g5, b5, wx):
+def gen(Z, w, g, b, w2, g2, b2, w3, g3, b3, w4, g4, b4, wx):
+#def gen(Z, w, g, b, w2, g2, b2, w3, g3, b3, w4, g4, b4, w5, g5, b5, wx):
 #def gen(Z, w, g, b, w2, g2, b2, w3, g3, b3, w4, g4, b4, w5, g5, b5, w6, g6, b6, wx):
     h = relu(batchnorm(T.dot(Z, w), g=g, b=b))
-    h = h.reshape((h.shape[0], ngf*8, 4, 4))
+    h = h.reshape((h.shape[0], ngf, 4, 4))
     h2 = relu(batchnorm(deconv(h, w2, subsample=(2, 2), border_mode=(2, 2)), g=g2, b=b2))
     h3 = relu(batchnorm(deconv(h2, w3, subsample=(2, 2), border_mode=(2, 2)), g=g3, b=b3))
     h4 = relu(batchnorm(deconv(h3, w4, subsample=(2, 2), border_mode=(2, 2)), g=g4, b=b4))
-    x = tanh(deconv(h4, wx, subsample=(2, 2), border_mode=(2, 2)))
+    if IMG_SIZE==64:
+        x = tanh(deconv(h4, wx, subsample=(2, 2), border_mode=(2, 2)))
+    elif IMG_SIZE==128:
+        h5 = relu(batchnorm(deconv(h4, w5, subsample=(2, 2), border_mode=(2, 2)), g=g5, b=b5)) #128
+        x = tanh(deconv(h5, wx, subsample=(2, 2), border_mode=(2, 2)))
+    elif IMG_SIZE==256:
+        h5 = relu(batchnorm(deconv(h4, w5, subsample=(2, 2), border_mode=(2, 2)), g=g5, b=b5)) #128
+        h6 = relu(batchnorm(deconv(h5, w6, subsample=(2, 2), border_mode=(2, 2)), g=g6, b=b6)) #256
+        x = tanh(deconv(h6, wx, subsample=(2, 2), border_mode=(2, 2)))
     return x
     
 Z = T.matrix()
